a25e32a2a7ae6a1a995fec7cd17d548ed8f986ac
Jennifer Rush <jennifer.rush2@gmail.com>
2023-12-16 18:13:06 -0700

    adds and tests link and info for show page

--- spec/features/songs/show_spec.rb
+++ spec/features/songs/show_spec.rb
@@ -19,9 +19,33 @@ RSpec.describe 'the songs show page' do
         song_2 = artist.songs.create!(title: "Call Me Maybe", length: 207, play_count: 21456357)
         
         visit "/songs/#{song.id}"
-        save_and_open_page
         expect(page).to have_content(artist.name)
 
+    end
+
+    it 'displays the song length' do
+        artist = Artist.create!(name: 'Carly Rae Jepsen')
+        song = artist.songs.create!(title: "I Really Like You", length: 208, play_count: 2456357)
+        song_2 = artist.songs.create!(title: "Call Me Maybe", length: 207, play_count: 21456357)
+        
+        visit "/songs/#{song.id}"
+
+        expect(page).to have_content(song.length)
+        expect(page).to_not have_content(song_2.length)
+    end
+
+    it 'has a link back to the index page' do
+        artist = Artist.create!(name: 'Carly Rae Jepsen')
+        song = artist.songs.create!(title: "I Really Like You", length: 208, play_count: 2456357)
+        song_2 = artist.songs.create!(title: "Call Me Maybe", length: 207, play_count: 21456357)
+        
+        visit "/songs/#{song.id}"
+        #save_and_open_page
+        expect(page).to have_link("Back to Song List", href: '/songs')
+
+        click_link "Back to Song List"
+
+        expect(current_path).to eq('/songs')
     end
 
 end
