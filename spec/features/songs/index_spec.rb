1c88508f2a887af326c0989674b24ab9fd891571
Jennifer Rush <jennifer.rush2@gmail.com>
2023-12-16 17:48:00 -0700

    creates test for songs index page

---
+++ spec/features/songs/index_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe "songs index page", type: :feature do
+    it "can see all songs titles and play count" do
+        carly = Artist.create(name: "Carly Rae Jepson")
+        song_1 = Song.create!(title: "I Really Like You",
+                             length: 208,
+                             play_count: 243810867,
+                             artist_id: carly.id)
+        song_2 = Song.create!(title: "Call Me Maybe",
+                             length: 199,
+                             play_count: 1214722172,
+                             artist_id: carly.id)
+        
+        visit "/songs"
+
+        expect(page).to have_content(song_1.title)
+        expect(page).to have_content("Play Count: #{song_1.play_count}")
+        expect(page).to have_content(song_2.title)
+        expect(page).to have_content("Play Count: #{song_2.play_count}")
+    end
+
+    it "has a header 'all songs'" do
+        carly = Artist.create(name: "Carly Rae Jepson")
+        song_1 = Song.create!(title: "I Really Like You",
+                             length: 208,
+                             play_count: 243810867,
+                             artist_id: carly.id)
+        song_2 = Song.create!(title: "Call Me Maybe",
+                             length: 199,
+                             play_count: 1214722172,
+                             artist_id: carly.id)
+        
+        visit "/songs"
+        save_and_open_page
+        expect(page).to have_selector('h1', text: 'Songs List')
+
+    end
+
+end
