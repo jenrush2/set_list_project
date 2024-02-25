a1e8e1310e9cb0bf90116fdc72ee851d5faceac9
Jennifer Rush <jennifer.rush2@gmail.com>
2024-02-24 18:34:59 -0700

    creates instance and class methods for artist

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,7 +3,19 @@ class Artist < ApplicationRecord
     validates_presence_of :name
 
     def average_song_length
-        self.songs.average(:length)
+        songs.average(:length)
+    end
+
+    def last_updated
+        updated_at.strftime("%Y-%m-%d")
+    end
+
+    def self.newest_first
+        order(created_at: :desc)
+    end
+
+    def self.by_name
+        order(:name)
     end
 
 end
