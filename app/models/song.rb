6498eacbab4f86199238665c97c10167de17a543
Jennifer Rush <jennifer.rush2@gmail.com>
2024-03-18 12:11:16 -0600

    adds playlist and playlist song associations

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,8 @@
 class Song < ApplicationRecord
     belongs_to :artist
+    has_many :playlist_songs
+    has_many :playlists, through: :playlist_songs 
+    
 
     def self.song_count
         all.count
