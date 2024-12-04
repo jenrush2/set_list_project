e3bde1568033c89a6b5ac25f5f7dc9c61c69de66
Jennifer Rush <jennifer.rush2@gmail.com>
2024-12-04 12:36:27 -0700

    finish updating routes with resources

--- config/routes.rb
+++ config/routes.rb
@@ -4,18 +4,26 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :songs, only: [:index, :show]
-  #get '/songs', to: 'songs#index'
-  #get '/songs/:id', to: 'songs#show'
-  get '/artists/:artist_id/songs', to: 'artist_songs#index'
-  resources :artists, only: [:index, :new, :create, :edit, :update, :destroy]
+  #before refactoring using resources:
+  #get '/artists/:artist_id/songs', to: 'artist_songs#index'
   #get '/artists', to: 'artists#index'
   #get '/artists/new', to: 'artists#new'
   #post '/artists', to: 'artists#create'
   #get '/artists/:id/edit', to: 'artists#edit'
   #patch '/artists/:id', to: 'artists#update'
   #delete '/artists/:id', to: 'artists#destroy'
-  get '/playlists', to: 'playlists#index'
+  #get '/songs', to: 'songs#index'
+  #get '/songs/:id', to: 'songs#show'
+  #get '/playlists', to: 'playlists#index'
+  
+  resources :songs, only: [:index, :show]
+
+  resources :artists do
+    resources :songs, only: [:index], controller: "artist_songs"
+  end
+  
+  resources :playlists, only: [:index]
+  
   patch '/cart/:song_id', to: 'cart#update'
   
 
