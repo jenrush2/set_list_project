71ad90882400d35040d0b858618d7361e9362867
Jennifer Rush <jennifer.rush2@gmail.com>
2024-12-04 12:20:18 -0700

    refactored routes using resources

--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,15 @@ Rails.application.routes.draw do
   get '/songs', to: 'songs#index'
   get '/songs/:id', to: 'songs#show'
   get '/artists/:artist_id/songs', to: 'artist_songs#index'
-  get '/artists', to: 'artists#index'
-  get '/artists/new', to: 'artists#new'
-  post '/artists', to: 'artists#create'
-  get '/artists/:id/edit', to: 'artists#edit'
-  patch '/artists/:id', to: 'artists#update'
-  delete '/artists/:id', to: 'artists#destroy'
+  resources :artists, only: [:index, :new, :create, :edit, :update, :destroy]
+  #get '/artists', to: 'artists#index'
+  #get '/artists/new', to: 'artists#new'
+  #post '/artists', to: 'artists#create'
+  #get '/artists/:id/edit', to: 'artists#edit'
+  #patch '/artists/:id', to: 'artists#update'
+  #delete '/artists/:id', to: 'artists#destroy'
   get '/playlists', to: 'playlists#index'
   patch '/cart/:song_id', to: 'cart#update'
+  
 
 end
